8bde7230fd00023b37702e6bf5087643796ac7dc
LC <Liuming.Chen@mail.citytech.cuny.edu>
2020-04-10 15:45:49 -0400

    Update MainWindow.cs
    completed all the required functions of the main window

--- AgileProject/Forms/MainWindow.cs
+++ AgileProject/Forms/MainWindow.cs
@@ -1,5 +1,6 @@
 ﻿using AgileProject.DAO;
 using AgileProject.Model;
+using AgileProject.Models;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
@@ -14,27 +15,71 @@ namespace AgileProject.Forms
 {
     public partial class MainWindow : Form
     {
-        public MainWindow()
+        private readonly int accountId;
+
+        public MainWindow(int accountId)
         {
+            this.accountId = accountId;
             InitializeComponent();
         }
 
-
         private void Form1_Load(object sender, EventArgs e)
         {
+            this.CenterToScreen();
+            if (accountId == 0) this.Close();
+
+            Account account = (new UserDAO()).GetAccountInfo(accountId);
+            lbUserName.Text = account.firstName + " " + account.lastName;
+
             MovieDAO movieDAO = new MovieDAO();
             loadMovies(movieDAO.GetAllMovieList());
-            movieDAO.GetGenreStringList().ForEach(g => cbbGenre.Items.Add(g));
+            loadGenres(movieDAO.GetGenreStringList());
         }
 
-        private void btnHome_Click(object sender, EventArgs e)
+        private void loadGenres(List<String> genres)
         {
-            pnMovies.Controls.Clear();
-            loadMovies((new MovieDAO()).GetAllMovieList());
+            int y = 0;
+            Font font = new Font("Microsoft Sans Serif", 18.00F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            genres.ForEach(g =>
+            {
+                pnGenre.Height += 30;
+                Label genre = new Label();
+                genre.Cursor = Cursors.Hand;
+                genre.Font = font;
+                genre.BackColor = Color.Black;
+                genre.ForeColor = Color.White;
+                genre.Size = new Size(120, 30);
+                genre.Location = new Point(0, y);
+                genre.Text = g;
+                genre.Click += new EventHandler(genreClick);
+                genre.MouseHover += new EventHandler(genreMenuMouseHover);
+                genre.MouseLeave += new EventHandler(genreMenuMouseLeave);
+                y += 30;
+                pnGenre.Controls.Add(genre);
+
+            });
+        }
+
+        private void genreClick(object sender, EventArgs e)
+        {
+            pnGenre.Visible = pnGenre.Visible == true ? false : true;
+            loadMovies((new MovieDAO()).GetGenreMoviesList(((Label) sender).Text));
+        }
+
+        private void genreMenuMouseHover(object sender, EventArgs e)
+        {
+            ((Label)sender).BackColor = ColorTranslator.FromHtml("#2b2b2b");
+        }
+
+        private void genreMenuMouseLeave(object sender, EventArgs e)
+        {
+            ((Label)sender).BackColor = Color.Black;
         }
 
         private void loadMovies(List<Movie> list)
         {
+            pnMovies.Controls.Clear();
+
             int x = 10, y = 10;
 
             list.ForEach(m =>
@@ -60,6 +105,7 @@ namespace AgileProject.Forms
                 name.AutoSize = false;
                 name.AutoEllipsis = true;
                 name.Font = new Font("Microsoft Sans Serif", 9.0F, FontStyle.Bold, GraphicsUnit.Point, ((byte)(134)));
+                name.ForeColor = Color.White;
                 name.Location = new Point(x, y + 190);
                 name.Size = new Size(128, 30);
                 name.TabIndex = 6;
@@ -73,6 +119,7 @@ namespace AgileProject.Forms
                 Label genre = new Label();
                 pnMovies.Controls.Add(genre);
                 genre.AutoSize = false;
+                genre.ForeColor = Color.White;
                 genre.Location = new Point(x, y + 225);
                 genre.Size = new Size(60, 13);
                 genre.TabIndex = 6;
@@ -89,6 +136,7 @@ namespace AgileProject.Forms
                 Label price = new Label();
                 pnMovies.Controls.Add(price);
                 price.AutoSize = false;
+                price.ForeColor = Color.White;
                 price.Location = new Point(x + 88, y + 225);
                 price.Size = new Size(40, 13);
                 price.TabIndex = 6;
@@ -100,33 +148,121 @@ namespace AgileProject.Forms
             });
         }
 
-        private void cbbGenre_SelectedIndexChanged(object sender, EventArgs e)
-        {
-            pnMovies.Controls.Clear();
-            loadMovies((new MovieDAO()).GetGenreMoviesList(cbbGenre.Text));
-        }
-
-        private void openMovieInfoFrom()
-        {
-
-        }
-
-        private void btnMyList_Click(object sender, EventArgs e)
-        {
-            //MovieInfo mi = new MovieInfo();
-            //mi.ShowDialog();
-        }
-
         private void movieInfoOpen_Click(object sender, EventArgs e, String movieId)
         {
-            //MovieInfo mi = new MovieInfo();
-            //mi.ShowDialog();
+            MovieInfoWindow movieInfoWindow = new MovieInfoWindow(movieId, accountId);
+            movieInfoWindow.ShowDialog();
         }
 
-        private void cbbGenre_MouseHover(object sender, EventArgs e)
+        private void lblHome_Click(object sender, EventArgs e)
         {
-            var box = sender as ComboBox;
-            box.DroppedDown = true;
+            loadMovies((new MovieDAO()).GetAllMovieList());
+        }
+
+        private void lblGenre_Click(object sender, EventArgs e)
+        {
+            pnGenre.Visible = pnGenre.Visible == true ? false : true;
+        }
+
+        /// <summary>
+        /// Hide Caret from Textbox
+        /// </summary>
+        /// <param name="hWnd"></param>
+        /// <returns></returns>
+        [System.Runtime.InteropServices.DllImport("user32.dll")]
+        private static extern bool HideCaret(IntPtr hWnd);
+
+        /// <summary>
+        /// Show Caret in Textbox
+        /// </summary>
+        /// <param name="hWnd"></param>
+        /// <returns></returns>
+        [System.Runtime.InteropServices.DllImport("user32.dll")]
+        private static extern bool ShowCaret(IntPtr hWnd);
+
+        private void tbSearch_Enter(object sender, EventArgs e)
+        {
+            tbSearch.Clear();
+            lbSearchBoxUnderline.Height = 3;
+        }
+
+        private void lbSearch_Click(object sender, EventArgs e)
+        {
+            HideCaret(tbSearch.Handle);
+            lbSearchBoxUnderline.Height = 1;
+            loadMovies((new MovieDAO()).SearchMovies(tbSearch.Text));
+        }
+
+        private void tbSearch_Click(object sender, EventArgs e)
+        {
+            ShowCaret(tbSearch.Handle);
+        }
+
+        private void lbSearch_MouseHover(object sender, EventArgs e)
+        {
+            lbSearch.BackColor = ColorTranslator.FromHtml("#2b2b2b");
+        }
+
+        private void lbSearch_MouseLeave(object sender, EventArgs e)
+        {
+            lbSearch.BackColor = Color.Black;
+        }
+
+        private void pbUserIcon_Click(object sender, EventArgs e)
+        {
+            pnUserOptions.Visible = pnUserOptions.Visible == true ? false : true;
+        }
+
+        private void lbUserSetting_Click(object sender, EventArgs e)
+        {
+            UserSettingWindow userSetting = new UserSettingWindow();
+            userSetting.Show();
+        }
+
+        private void lbSignOut_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+            LoginWindow login = new LoginWindow();
+            login.FormClosed += (s, args) => this.Close();
+            login.Show();
+        }
+
+        private void lbUserSetting_MouseHover(object sender, EventArgs e)
+        {
+            lbUserSetting.BackColor = Color.DimGray;
+        }
+
+        private void lbUserSetting_MouseLeave(object sender, EventArgs e)
+        {
+            lbUserSetting.BackColor = Color.Black;
+        }
+
+        private void lbSignOut_MouseHover(object sender, EventArgs e)
+        {
+            lbSignOut.BackColor = Color.DimGray;
+        }
+
+        private void lbSignOut_MouseLeave(object sender, EventArgs e)
+        {
+            lbSignOut.BackColor = Color.Black;
+        }
+
+        private void tbSearch_KeyDown(object sender, KeyEventArgs e)
+        {
+            if(e.KeyCode == Keys.Enter)
+            {
+                lbSearch_Click(this, new EventArgs());
+            }
+        }
+
+        private void lblMyList_Click(object sender, EventArgs e)
+        {
+            loadMovies((new MyListDAO()).GetMyList(accountId));
+        }
+
+        private void lbMyMovies_Click(object sender, EventArgs e)
+        {
+            loadMovies((new Order()).OrderList(accountId));
         }
     }
 }
