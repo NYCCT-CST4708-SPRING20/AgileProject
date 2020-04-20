d76400b73a8093a2112d08c02d44991d1b81e391
LC <Liuming.Chen@mail.citytech.cuny.edu>
2020-04-20 19:18:34 -0400

    Refactor & Redesign RegistrationWindow

--- AgileProject/Forms/RegistrationWindow.cs
+++ AgileProject/Forms/RegistrationWindow.cs
@@ -1,32 +1,135 @@
-﻿using System;
+﻿using AgileProject.DAO;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.Globalization;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace AgileProject.Forms
 {
     public partial class RegistrationWindow : Form
-
-
     {
         public RegistrationWindow()
         {
             InitializeComponent();
         }
 
-        private void button1_Click(object sender, EventArgs e)
+        private void btnSignUp_Click(object sender, EventArgs e)
         {
-            MessageBox.Show("A new Account has been created successfully");
+            if (String.IsNullOrWhiteSpace(tbFirstName.Text) ||
+               String.IsNullOrWhiteSpace(tbLastName.Text) ||
+               String.IsNullOrWhiteSpace(tbEmailAddress.Text) ||
+               String.IsNullOrWhiteSpace(tbConfirmEmail.Text) ||
+               String.IsNullOrWhiteSpace(tbPassword.Text) ||
+               String.IsNullOrWhiteSpace(tbConfirmPw.Text))
+            {
+                MessageBox.Show("One of the field is empty, please check!");
+                return;
+            }
+
+
+
+            UserDAO userDAO = new UserDAO();
+
+            if (tbEmailAddress.Text == tbConfirmEmail.Text)
+            {
+                if (!IsValidEmail(tbEmailAddress.Text))
+                {
+                    MessageBox.Show("Invalid email format");
+                    return;
+                }
+
+                if (userDAO.FindEmail(tbEmailAddress.Text))
+                {
+                    MessageBox.Show("This email is already used by another account!");
+                }
+                else
+                {
+                    if (tbPassword.Text == tbConfirmPw.Text)
+                    {
+                        userDAO.AddNewUser(tbFirstName.Text, tbLastName.Text, tbEmailAddress.Text, tbPassword.Text);
+                        MessageBox.Show("Sign Up Successfully!\nClick OK to go back to the login window!");
+                        this.Hide();
+                        LoginWindow loginWindow = new LoginWindow();
+                        loginWindow.FormClosed += (s, args) => this.Close();
+                        loginWindow.Show();
+                    }
+                    else
+                    {
+                        MessageBox.Show("Password do no match!");
+                    }
+                }
+            }
+            else
+            {
+                MessageBox.Show("Email do not match!");
+            }
         }
 
-        private void textBox4_TextChanged(object sender, EventArgs e)
+        private void RegistrationWindow_Load(object sender, EventArgs e)
         {
+            this.CenterToScreen();
+            lbLogin.MouseEnter += (s, args) => lbLogin.Font = new Font(lbLogin.Font.Name, lbLogin.Font.SizeInPoints, FontStyle.Underline);
+            lbLogin.MouseLeave += (s, args) => lbLogin.Font = new Font(lbLogin.Font.Name, lbLogin.Font.SizeInPoints, FontStyle.Regular);
+        }
 
+        private void lbLogin_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+            LoginWindow loginWindow = new LoginWindow();
+            loginWindow.FormClosed += (s, args) => this.Close();
+            loginWindow.Show();
+        }
+
+        private static bool IsValidEmail(string email)
+        {
+            if (string.IsNullOrWhiteSpace(email))
+                return false;
+
+            try
+            {
+                // Normalize the domain
+                email = Regex.Replace(email, @"(@)(.+)$", DomainMapper,
+                                      RegexOptions.None, TimeSpan.FromMilliseconds(200));
+
+                // Examines the domain part of the email and normalizes it.
+                string DomainMapper(Match match)
+                {
+                    // Use IdnMapping class to convert Unicode domain names.
+                    var idn = new IdnMapping();
+
+                    // Pull out and process domain name (throws ArgumentException on invalid)
+                    var domainName = idn.GetAscii(match.Groups[2].Value);
+
+                    return match.Groups[1].Value + domainName;
+                }
+            }
+            catch (RegexMatchTimeoutException e)
+            {
+                return false;
+            }
+            catch (ArgumentException e)
+            {
+                return false;
+            }
+
+            try
+            {
+                return Regex.IsMatch(email,
+                    @"^(?("")("".+?(?<!\\)""@)|(([0-9a-z]((\.(?!\.))|[-!#\$%&'\*\+/=\?\^`\{\}\|~\w])*)(?<=[0-9a-z])@))" +
+                    @"(?(\[)(\[(\d{1,3}\.){3}\d{1,3}\])|(([0-9a-z][-0-9a-z]*[0-9a-z]*\.)+[a-z0-9][\-a-z0-9]{0,22}[a-z0-9]))$",
+                    RegexOptions.IgnoreCase, TimeSpan.FromMilliseconds(250));
+            }
+            catch (RegexMatchTimeoutException)
+            {
+                return false;
+            }
         }
     }
 }
